5e5562b86541bf8b6847b6179dbc50b535d699eb
손옥무 | son okmoo <94339333+okdoittttt@users.noreply.github.com>
2022-05-17 17:56:14 +0900

    Add files via upload

---
+++ bearOrhuman/sketch.js
@@ -0,0 +1,73 @@
+
+  // Classifier Variable
+  let classifier;
+  // Model URL
+  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/j6qw4RcO2/';
+  
+  // Video
+  let video;
+  let flippedVideo;
+  // To store the classification
+  let label = "";
+
+  // Load the model first
+  function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  }
+
+  function setup() {
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+  }
+
+  function draw() {
+    background(0);
+    // Draw the video
+    image(flippedVideo, 0, 0);
+
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+  }
+
+  if(label == "사람") {
+    document.getElementById("hi").innerHTML = "사람입니다.";
+    text(label, width / 2, height - 4);
+  }else if(label == "곰돌이"){
+    document.getElementById("hi").innerHTML = "곰돌입니다.";
+    text(label, width / 2, height - 4);
+  }else {
+    document.getElementById("hi").innerHTML = "인식할 수 없습니다.";
+    text(label, width / 2, height - 4);
+  }
+
+  // Get a prediction for the current video frame
+  function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+
+  }
+
+  // When we get a result
+  function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    // Classifiy again!
+    classifyVideo();
+  }
